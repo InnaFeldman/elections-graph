84ec3b2df3102a79e7a9a473800435b52bd97a37
inna <innafeldman01@gmail.com>
2021-04-07 20:51:06 +0300

    master

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 (function(){
     let ctx = document.getElementById('myChart').getContext('2d');
-    let labels = ['22', '23', '24'];
-    let data =  [10, 5, 2];
+    let labels = ['24', '23', '22'];
+    var data = [];
+
+    for(let i = 0; i <= labels.length-1; i++){
+
+        let election_24 = $.ajax({
+            url: `https://israel-elections-1.s3.eu-west-3.amazonaws.com/${labels[i]}/allResults.json`,
+            type: "GET",
+            success: function(result) {
+                if(result){
+                    let number = Object.keys(result.realResults).length;
+                    data.push(number);
+                }else {
+                    console.log('Error')
+                }
+            }
+        })
+    }
+
+    console.log(data);
     
     var chart = new Chart(ctx, {
         type: 'bar',
         data: {
            labels: labels,
               datasets: [{
-                label: 'My First dataset',
-                backgroundColor: 'rgb(255, 99, 132)',
+                label: '3 last elections',
+                backgroundColor: '#6a5acd',
                 borderColor: 'rgb(255, 99, 132)',
                 data: data,
               }]
@@ -20,24 +38,5 @@
             }
         }
     })
-    
-    // const Http = new XMLHttpRequest();
-    // const url='https://israel-elections-1.s3.eu-west-3.amazonaws.com/22/allResults.json?v=0.18408311438933267';
-    // Http.open("GET", url);
-    // Http.send();
-    
-    // Http.onreadystatechange = (e) => {
-    //   console.log(Http.responseText)
-    // }
-    const url='https://israel-elections-1.s3.eu-west-3.amazonaws.com/22/allResults.json?v=0.18408311438933267';
-    $.ajax({
-        url: url,
-        type: "GET",
-        success: function(result) {
-            console.log(result.realResults)
-        },
-        error: function(error) {
-            console.log(`Error ${error}`)
-        }
-    })
+
 })()
