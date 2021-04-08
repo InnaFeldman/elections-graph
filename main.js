84fc30eae342b47af2d0c5a8bcd3f22e2148e148
Shani Peleg <shani28s5@gmail.com>
2021-04-08 11:07:18 +0300

    master

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 (function(){
     let ctx = document.getElementById('myChart').getContext('2d');
     let labels = ['22', '23', '24'];
-    var data = ['10', '5', '7'];
-
+    var newdata = [];
 
     for(let i = 0; i <= labels.length; i++){
-
         this["ajax_"+i] = {
         url: `https://israel-elections-1.s3.eu-west-3.amazonaws.com/${labels[i]}/allResults.json`,
         type: "GET",
         success: function(result) {
             if(result){
                 let number = Object.keys(result.realResults).length;
-                console.log(number);
+                newdata.push(number);
+                console.log(newdata);
             }else {
                 console.log('Error')
             }
@@ -20,37 +19,32 @@
     }
     }
 
-
     const ajaxCalls = [ajax_0, ajax_1, ajax_2].filter(a => a !== null);
 
     Promise.all(ajaxCalls.map(a =>jQuery.ajax(a)))
     .then(data => {
-        console.log("all finished", data);//used fro debug//* This is for DEBUG!.
+        console.log("all finished", data);
         
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+            labels: labels,
+                datasets: [{
+                    label: '3 last elections',
+                    backgroundColor: '#6a5acd',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: newdata,
+                }]
+            },
+            options: {
+                tooltips: {
+                    mode: 'index',
+                }
+            }
+        })
     }).catch(err=> {
         // something went wrong show error in console
-        console.log(new Error(err));//* This is for DEBUG!.
-    })
-
-
-
-    
-    var chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-           labels: labels,
-              datasets: [{
-                label: '3 last elections',
-                backgroundColor: '#6a5acd',
-                borderColor: 'rgb(255, 99, 132)',
-                data: data,
-              }]
-        },
-        options: {
-            tooltips: {
-                mode: 'index',
-            }
-        }
+        console.log(new Error(err));
     })
 
 })()
